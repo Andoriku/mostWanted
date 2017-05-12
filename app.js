00c24549aa608f34ba44c0546eb92185744b1ff1
Andoriku <Andoriku@gmail.com>
2017-05-12 14:02:51 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,97 @@
+/*
+Build all of your functions for displaying and gathering information below (GUI).
+*/
+
+// app is the function called to start the entire application
+function app(people){
+  var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
+  switch(searchType){
+    case 'yes':
+    var person = searchByName(people);
+    mainMenu(person, people);
+    break;
+    case 'no':
+    var person = searchByTrait();
+    return person;
+    break;
+    default:
+    app(people); // restart app
+    break;
+  }
+}
+
+// Menu function to call once you find who you are looking for
+function mainMenu(person, people){
+
+  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
+
+  if(!person){
+    alert("Could not find that individual.");
+    return app(people); // restart
+  }
+
+  var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+
+  switch(displayOption){
+    case "info":
+    displayPerson(person);
+    break;
+    case "family":
+    // TODO: get person's family
+    break;
+    case "descendants":
+    // TODO: get person's descendants
+    break;
+    case "restart":
+    app(people); // restart
+    break;
+    case "quit":
+    return; // stop execution
+    default:
+    return mainMenu(person, people); // ask again
+  }
+}
+
+function searchByName(people){
+  var firstName = promptFor("What is the person's first name?", chars);
+  var lastName = promptFor("What is the person's last name?", chars);
+  for (var i = 0; i < people.length; i++) {
+    if ((firstName + lastName) === (people[i].firstName + people[i].lastName)) {
+      var person = people[i];
+      return person;
+    }
+  }
+}
+// alerts a list of people
+function displayPeople(people){
+  alert(people.map(function(person){
+    return person.firstName + " " + person.lastName;
+  }).join("\n"));
+}
+
+function displayPerson(person){
+  // print all of the information about a person:
+  // height, weight, age, name, occupation, eye color.
+  var personInfo = "First Name: " + person.firstName + "\n";
+  personInfo += "Last Name: " + person.lastName + "\n";
+  // TODO: finish getting the rest of the information to display
+  alert(personInfo);
+}
+
+// function that prompts and validates user input (can be used as a prompt for any new input inquiries)
+function promptFor(question, valid){
+  do{
+    var response = prompt(question).trim();
+  } while(!response || !valid(response));
+  return response;
+}
+
+// helper function to pass into promptFor to validate yes/no answers
+function yesNo(input){
+  return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
+}
+
+// helper function any input into promptFor() from user is valid input
+function chars(input){
+  return true; // default validation only
+}
